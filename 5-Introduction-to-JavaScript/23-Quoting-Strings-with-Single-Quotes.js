18d07b98687427a8d9dc3998fb63ef5c709db6f3
padmalatha kuppa <padmalathakuppa411@gmail.com>
2019-03-22 12:04:48 -0600

    Adds code to the twenty-third lesson in the Javascript-Code module

---
+++ 5-Introduction-to-JavaScript/23-Quoting-Strings-with-Single-Quotes.js
@@ -0,0 +1,2 @@
+//I learned how to include a conversational string inside a string without using a backslash by quoting string value with single quotes.
+var myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
