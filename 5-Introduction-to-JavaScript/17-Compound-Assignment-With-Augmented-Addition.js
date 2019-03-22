07edf5aabc36a6bd5755753f0405fc4bfbb3150d
padmalatha kuppa <padmalathakuppa411@gmail.com>
2019-03-22 11:55:23 -0600

    Adds code to the seventh lesson in the Javascript-Code module

---
+++ 5-Introduction-to-JavaScript/17-Compound-Assignment-With-Augmented-Addition.js
@@ -0,0 +1,10 @@
+//I learned how to use a compound assignment with augmented addition using += operator.
+var a = 3;
+var b = 17;
+var c = 12;
+
+// Only modify code below this line
+
+a += 12;
+b += 9;
+c += 7;
