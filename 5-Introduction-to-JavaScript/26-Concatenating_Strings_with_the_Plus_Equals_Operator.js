1254b0c77ba2e78f0044fa4147ae4693561ac26f
padmalatha kuppa <padmalathakuppa411@gmail.com>
2019-03-22 12:10:27 -0600

    Adds code to the twenty-sixth lesson in the Javascript-Code module

---
+++ 5-Introduction-to-JavaScript/26-Concatenating_Strings_with_the_Plus_Equals_Operator.js
@@ -0,0 +1,9 @@
+//I learned how to concatenate strings with the += operator.
+// Example
+var ourStr = "I come first. ";
+ourStr += "I come second.";
+
+// Only change code below this line
+
+var myStr = "This is the first sentence. ";
+myStr += "This is the second sentence.";
