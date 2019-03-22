c968336969b2b15a2510b9f5ba2d263510ab12ec
padmalatha kuppa <padmalathakuppa411@gmail.com>
2019-03-22 12:00:14 -0600

    Adds code to the twentieth lesson in the Javascript-Code module

---
+++ 5-Introduction-to-JavaScript/20-Compound-Assignment-With-Augmented-Division.js
@@ -0,0 +1,10 @@
+//I learned how to use a compound assignment with augmented division using /=operator.
+var a = 48;
+var b = 108;
+var c = 33;
+
+// Only modify code below this line
+
+a /= 12;
+b /= 4;
+c /= 11;
