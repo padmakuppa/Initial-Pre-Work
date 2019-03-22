c36a7ff5de718dd308f40d8e357dd4dff78db7d0
padmalatha kuppa <padmalathakuppa411@gmail.com>
2019-03-22 12:15:53 -0600

    Adds code to the thirtieth lesson in the Javascript-Code module

---
+++ 5-Introduction-to-JavaScript/30-Use_Bracket_Notation_to_Find_the_First_Character_in_a_String.js
@@ -0,0 +1,12 @@
+//I learned about bracket notation and used it to get a character at a specific index within a string.
+var firstLetterOfFirstName = "";
+var firstName = "Ada";
+
+firstLetterOfFirstName = firstName[0];
+
+// Setup
+var firstLetterOfLastName = "";
+var lastName = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName[0];
