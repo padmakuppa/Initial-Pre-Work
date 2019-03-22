fba8d5f3a2015fbba9b8141f03dad5e49b556485
padmalatha kuppa <padmalathakuppa411@gmail.com>
2019-03-21 22:12:39 -0600

    Adds code to the second lesson in the Javascript-Code module

---
+++ 5-Introduction-to-JavaScript/2-Declare-JavaScript-Variables.js
@@ -0,0 +1,6 @@
+//I learned how many data types are in javascript and how to declare a variable by putting the keyword ‘var’ in front of it.
+// Example
+var ourName;
+
+// Declare myName below this line
+var myName;
