6893cfae6c3c4e905e3453da5bfa9290d95a978b
padmalatha kuppa <padmalathakuppa411@gmail.com>
2019-03-22 12:13:37 -0600

    Adds code to the twenty-eighth lesson in the Javascript-Code module

---
+++ 5-Introduction-to-JavaScript/28-Appending_Variables_to_Strings.js
@@ -0,0 +1,11 @@
+//I learned how to append variables to strings using += operator.
+// Example
+var anAdjective = "awesome!";
+var ourStr = "freeCodeCamp is ";
+ourStr += anAdjective;
+
+// Only change code below this line
+
+var someAdjective = "Awesome!";
+var myStr = "Learning to code is ";
+myStr += someAdjective;
