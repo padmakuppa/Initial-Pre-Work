f0c93c519c888e508a61a576d9e31c826cd9062d
padmalatha kuppa <padmalathakuppa411@gmail.com>
2019-03-22 12:02:14 -0600

    Adds code to the twenty-first lesson in the Javascript-Code module

---
+++ 5-Introduction-to-JavaScript/21-Declare-String-Variables.js
@@ -0,0 +1,8 @@
+//I learned how to declare string variables in Javascript.
+// Example
+var firstName = "Alan";
+var lastName = "Turing";
+
+// Only change code below this line
+var myFirstName = "bubsy";
+var myLastName  = "mynti";
