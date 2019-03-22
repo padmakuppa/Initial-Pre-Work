a781ad5a02148597c3d0ea9e8444df8e51d5fe16
padmalatha kuppa <padmalathakuppa411@gmail.com>
2019-03-22 12:11:11 -0600

    Adds code to the twenty-seventh lesson in the Javascript-Code module

---
+++ 5-Introduction-to-JavaScript/27-Constructing_Strings_with_Variables.js
@@ -0,0 +1,8 @@
+//I learned how to construct a string with variables by inserting them using a + operator.
+// Example
+var ourName = "freeCodeCamp";
+var ourStr = "Hello, our name is " + ourName + ", how are you?";
+
+// Only change code below this line
+var myName = "bubsy";
+var myStr = "My name is " + myName + " and I am wll!";
