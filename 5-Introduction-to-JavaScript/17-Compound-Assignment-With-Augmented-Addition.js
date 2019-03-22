f8b4bbefc16dc4ea81e2bcc6d72fa739c4106a24
padmalatha kuppa <padmalathakuppa411@gmail.com>
2019-03-22 12:21:06 -0600

    Adds code to the seventeenth lesson in the Javascript-Code module

--- 5-Introduction-to-JavaScript/17-Compound-Assignment-With-Augmented-Addition.js
+++ 5-Introduction-to-JavaScript/17-Compound-Assignment-With-Augmented-Addition.js
@@ -1,4 +1,4 @@
-//I learned how to use a compound assignment with augmented addition using += operator.
+//I learned how to use a compound assignment with augmented addition using the += operator.
 var a = 3;
 var b = 17;
 var c = 12;
