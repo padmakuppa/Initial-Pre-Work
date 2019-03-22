9b31eaf2f808cec1ca423b63f988ee212a5aabce
padmalatha kuppa <padmalathakuppa411@gmail.com>
2019-03-22 12:03:32 -0600

    Adds code to the twenty-second lesson in the Javascript-Code module

---
+++ 5-Introduction-to-JavaScript/22-Escaping-Literal-Quotes-in-Strings.js
@@ -0,0 +1,2 @@
+//A string value must start and end with a single or double quote. I learned how to include a literal quote inside a string using a backslash (\) in front of the quote.
+var myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
