c0d2774bf44d4fc3c59d2b4d3aa624103c0a2c38
padmalatha kuppa <padmalathakuppa411@gmail.com>
2019-03-24 10:24:02 -0600

    Adds code to the fifty-second lesson in the Javascript-Code module

---
+++ 5-Introduction-to-JavaScript/52-Understanding_Undefined_Value_returned_from_a_Function.js
@@ -0,0 +1,15 @@
+//I learned that if a function does not have a ‘return’ statement, the function process the inner code but the result would be undefined.
+// Example
+var sum = 0;
+function addThree() {
+  sum = sum + 3;
+}
+
+// Only change code below this line
+function addFive() {
+   sum = sum + 5;
+}
+
+
+// Only change code above this line
+var returnedValue = addF
