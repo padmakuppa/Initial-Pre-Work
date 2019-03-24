9708689fd7e2d1f9101235dff62b0c97581056a0
padmalatha kuppa <padmalathakuppa411@gmail.com>
2019-03-24 10:31:43 -0600

    Adds code to the fifty-seventh lesson in the Javascript-Code module

---
+++ 5-Introduction-to-JavaScript/57-Comparison_with_the_Equality_Operator.js
@@ -0,0 +1,11 @@
+//I learned about the comparison operator == in Javascript, and the equality operator would return Boolean values.
+// Setup
+function testEqual(val) {
+  if (val == 12) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+// Change this value to test
+testEqual(10);
