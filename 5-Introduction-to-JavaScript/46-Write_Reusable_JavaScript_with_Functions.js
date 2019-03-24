74cf599c753b178421d7cd66aa5a1bbb0ee2857f
padmalatha kuppa <padmalathakuppa411@gmail.com>
2019-03-24 10:15:31 -0600

    Adds code to the forty-sixth lesson in the Javascript-Code module

---
+++ 5-Introduction-to-JavaScript/46-Write_Reusable_JavaScript_with_Functions.js
@@ -0,0 +1,13 @@
+//I learned about Javascript functions and how they are reusable in the code.
+// Example
+function ourReusableFunction() {
+  console.log("Heyya, World");
+}
+
+ourReusableFunction();
+
+// Only change code below this line
+function reusableFunction() {
+  console.log("Hi World");
+}
+reusableFunction();
