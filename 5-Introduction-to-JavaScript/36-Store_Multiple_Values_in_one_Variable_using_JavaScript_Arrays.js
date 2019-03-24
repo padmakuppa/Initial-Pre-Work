38fbd0009acaa5fa5e60a7c45fa21d1fc8afaa33
padmalatha kuppa <padmalathakuppa411@gmail.com>
2019-03-24 09:56:16 -0600

    Adds code to the thirty-sixth lesson in the Javascript-Code module

---
+++ 5-Introduction-to-JavaScript/36-Store_Multiple_Values_in_one_Variable_using_JavaScript_Arrays.js
@@ -0,0 +1,6 @@
+//I learned how to store multiple values in one variable using Javascript arrays.
+// Example
+var ourArray = ["John", 23];
+
+// Only change code below this line.
+var myArray = ["Bob", 35];
