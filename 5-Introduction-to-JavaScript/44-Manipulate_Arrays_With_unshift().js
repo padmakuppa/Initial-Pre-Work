1c9f4de59c88853a6386df4380786f256aa89c5a
padmalatha kuppa <padmalathakuppa411@gmail.com>
2019-03-24 10:13:21 -0600

    Adds code to the forty-fourth lesson in the Javascript-Code module

---
+++ 5-Introduction-to-JavaScript/44-Manipulate_Arrays_With_unshift().js
@@ -0,0 +1,13 @@
+//I learned how to add values to the beginning of an array using unshift () function.
+// Example
+var ourArray = ["Stimpson", "J", "cat"];
+ourArray.shift(); // ourArray now equals ["J", "cat"]
+ourArray.unshift("Happy");
+// ourArray now equals ["Happy", "J", "cat"]
+
+// Setup
+var myArray = [["John", 23], ["dog", 3]];
+myArray.shift();
+
+// Only change code below this line.
+myArray.unshift(["Paul", 35]);
