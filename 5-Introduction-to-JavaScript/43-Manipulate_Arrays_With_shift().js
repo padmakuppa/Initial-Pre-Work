0cbf05e9d084413481fb132b915752bc8c2787fc
padmalatha kuppa <padmalathakuppa411@gmail.com>
2019-03-24 10:05:35 -0600

    Adds code to the forty-third lesson in the Javascript-Code module

---
+++ 5-Introduction-to-JavaScript/43-Manipulate_Arrays_With_shift().js
@@ -0,0 +1,11 @@
+//I learned how to remove the first value of an array using shift() function.
+// Example
+var ourArray = ["Stimpson", "J", ["cat"]];
+var removedFromOurArray = ourArray.shift();
+// removedFromOurArray now equals "Stimpson" and ourArray now equals ["J", ["cat"]].
+
+// Setup
+var myArray = [["John", 23], ["dog", 3]];
+
+// Only change code below this line.
+var removedFromMyArray = myArray.shift();
