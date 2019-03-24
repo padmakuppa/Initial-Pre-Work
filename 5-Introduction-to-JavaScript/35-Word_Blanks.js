a656b04979d6fe5ccd1f4e226b78f2b8b60e2f15
padmalatha kuppa <padmalathakuppa411@gmail.com>
2019-03-24 09:55:33 -0600

    Adds code to the thirty-fifth lesson in the Javascript-Code module

---
+++ 5-Introduction-to-JavaScript/35-Word_Blanks.js
@@ -0,0 +1,11 @@
+//I have completed a word blanks challenge by concatenating the provided variables with words of my choice to form a complete sentence.
+function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+  // Your code below this line
+  var result = "The " + myNoun + " was " + myAdjective + " and " + myVerb + " " + myAdverb;
+
+  // Your code above this line
+  return result;
+}
+
+// Change the words here to test your function
+wordBlanks("dog", "big", "ran", "quickly");
