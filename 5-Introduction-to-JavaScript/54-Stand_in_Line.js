f70d2dad5c3eb110436902cd5dd2b06785a5c80c
padmalatha kuppa <padmalathakuppa411@gmail.com>
2019-03-24 10:28:20 -0600

    Adds code to the fifty-fourth lesson in the Javascript-Code module

---
+++ 5-Introduction-to-JavaScript/54-Stand_in_Line.js
@@ -0,0 +1,14 @@
+//I have created a function nextInLine which adds a number at the end of the array and removes the first element of the array and returns the remvoed element.
+function nextInLine(arr, item) {
+  // Your code here
+  arr.push(item);
+  return arr.shift();  // Change this line
+}
+
+// Test Setup
+var testArr = [1,2,3,4,5];
+
+// Display Code
+console.log("Before: " + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 6)); // Modify this line to test
+console.log("After: " + JSON.stringify(testArr));
