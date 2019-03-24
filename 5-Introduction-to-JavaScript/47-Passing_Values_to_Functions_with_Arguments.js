4cb1beeebf76895a2a3d6221b47d526c24b99fc8
padmalatha kuppa <padmalathakuppa411@gmail.com>
2019-03-24 10:16:24 -0600

    Adds code to the forty-seventh lesson in the Javascript-Code module

---
+++ 5-Introduction-to-JavaScript/47-Passing_Values_to_Functions_with_Arguments.js
@@ -0,0 +1,13 @@
+//I learned about parameters and arguments where parameters are placeholder variables and arguments are actual values.
+// Example
+function ourFunctionWithArgs(a, b) {
+  console.log(a - b);
+}
+ourFunctionWithArgs(10, 5); // Outputs 5
+
+// Only change code below this line.
+function functionWithArgs(param1, param2) {
+  var sum = param1 + param2;
+  console.log(sum);
+}
+functionWithArgs(7,8);
