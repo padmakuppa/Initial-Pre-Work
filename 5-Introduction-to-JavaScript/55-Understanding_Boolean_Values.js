2874426dfc21abaf913bbccb030b363de54749d0
padmalatha kuppa <padmalathakuppa411@gmail.com>
2019-03-24 10:29:00 -0600

    Adds code to the fifty-fifth lesson in the Javascript-Code module

---
+++ 5-Introduction-to-JavaScript/55-Understanding_Boolean_Values.js
@@ -0,0 +1,9 @@
+//I learned about Boolean values (true, false) and how to use them in Javascript.
+function welcomeToBooleans() {
+
+// Only change code below this line.
+
+return true; // Change this line
+
+// Only change code above this line.
+}
