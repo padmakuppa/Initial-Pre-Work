9bba158c79b0e184cbaf87f131bc236d7cf90fd3
padmalatha kuppa <padmalathakuppa411@gmail.com>
2019-03-24 10:17:26 -0600

    Adds code to the forty-eighth lesson in the Javascript-Code module

---
+++ 5-Introduction-to-JavaScript/48-Global_Scope_and_Functions.js
@@ -0,0 +1,20 @@
+//I learned that variables without 'var' keyword are in Global scope and that can create unintended consequences. So variables should always be declared with 'var' keyword in functions.
+// Declare your variable here
+var myGlobal = 10;
+
+function fun1() {
+  // Assign 5 to oopsGlobal Here
+  oopsGlobal = 5;
+}
+
+// Only change code above this line
+function fun2() {
+  var output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+}
