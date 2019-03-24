b9d568d9c9ad6724caf6ba6a2c7636c5bcf636d9
padmalatha kuppa <padmalathakuppa411@gmail.com>
2019-03-24 10:21:37 -0600

    Adds code to the fifty-first lesson in the Javascript-Code module

---
+++ 5-Introduction-to-JavaScript/51-Return_a_Value_from_a_Function_with_Return.js
@@ -0,0 +1,13 @@
+//I learned how to send a value back out of a function using a 'return' statement.
+// Example
+function minusSeven(num) {
+  return num - 7;
+}
+
+// Only change code below this line
+function timesFive(num) {
+  return num * 5;
+}
+console.log(timesFive(16));
+
+console.log(minusSeven(10));
