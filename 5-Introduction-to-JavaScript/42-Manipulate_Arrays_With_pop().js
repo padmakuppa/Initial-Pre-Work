74059de5bd1c575da4cb1d5be80a2060162535fd
padmalatha kuppa <padmalathakuppa411@gmail.com>
2019-03-24 10:04:51 -0600

    Adds code to the forty-second lesson in the Javascript-Code module

---
+++ 5-Introduction-to-JavaScript/42-Manipulate_Arrays_With_pop().js
@@ -0,0 +1,11 @@
+//I learned how to remove a value off of the end of an array using a pop () function.
+// Example
+var ourArray = [1,2,3];
+var removedFromOurArray = ourArray.pop();
+// removedFromOurArray now equals 3, and ourArray now equals [1,2]
+
+// Setup
+var myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line.
+var removedFromMyArray = myArray.pop();
