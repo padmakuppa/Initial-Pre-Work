4ec930a90bc468b23acd5dc23058abdc8790fbee
padmalatha kuppa <padmalathakuppa411@gmail.com>
2019-03-24 09:58:16 -0600

    Adds code to the thirty-seventh lesson in the Javascript-Code module

---
+++ 5-Introduction-to-JavaScript/37-Nest_one_Array_within_Another_Array.js
@@ -0,0 +1,6 @@
+//I learned how to nest one array within another array.
+// Example
+var ourArray = [["the universe", 42], ["everything", 101010]];
+
+// Only change code below this line.
+var myArray = [["cats", 17],["dogs" , 6]];
