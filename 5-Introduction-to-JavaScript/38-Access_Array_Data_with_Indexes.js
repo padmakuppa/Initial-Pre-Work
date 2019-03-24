285478623e094201d1a2a5aa79aa38e8535fd6a8
padmalatha kuppa <padmalathakuppa411@gmail.com>
2019-03-24 10:00:03 -0600

    Adds code to the thirty-eighth lesson in the Javascript-Code module

---
+++ 5-Introduction-to-JavaScript/38-Access_Array_Data_with_Indexes.js
@@ -0,0 +1,10 @@
+//I learned how to access the data inside an array using indexes.
+// Example
+var ourArray = [50,60,70];
+var ourData = ourArray[0]; // equals 50
+
+// Setup
+var myArray = [50,60,70];
+
+// Only change code below this line.
+var myData = myArray[0];
