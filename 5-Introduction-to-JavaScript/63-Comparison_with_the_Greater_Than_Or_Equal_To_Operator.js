4851c1648e8f5aac5129012ff47004f84120e01a
padmalatha kuppa <padmalathakuppa411@gmail.com>
2019-03-25 20:32:00 -0600

    Adds code to the sixty-third lesson in the Javascript-Code module

---
+++ 5-Introduction-to-JavaScript/63-Comparison_with_the_Greater_Than_Or_Equal_To_Operator.js
@@ -0,0 +1,15 @@
+//I learned how to compare with the greater-than or equal to operator (>=). It will perform type conversions.
+function testGreaterOrEqual(val) {
+  if (val >= 20) {  // Change this line
+    return "20 or Over";
+  }
+
+  if (val >= 10) {  // Change this line
+    return "10 or Over";
+  }
+
+  return "Less than 10";
+}
+
+// Change this value to test
+testGreaterOrEqual(10);
