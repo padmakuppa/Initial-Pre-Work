d0cb58d12325b964219b8f4a51b8d26a0d16a6da
padmalatha kuppa <padmalathakuppa411@gmail.com>
2019-03-25 20:53:40 -0600

    Adds code to the eightieth lesson in the Javascript-Code module

---
+++ 5-Introduction-to-JavaScript/80-Build_JavaScript_Objects.js
@@ -0,0 +1,17 @@
+//I learned about Javascript objects and how to store data in objects through properties.
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+// Only change code below this line.
+
+var myDog = {
+  "name": "Snoopy",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["boby", "puppy"]
+};
