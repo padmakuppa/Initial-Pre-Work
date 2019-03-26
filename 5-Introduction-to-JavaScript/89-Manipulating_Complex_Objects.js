1e95b752d29dd69bae0b99cb65fc371a89b723b3
padmalatha kuppa <padmalathakuppa411@gmail.com>
2019-03-25 21:06:20 -0600

    Adds code to the eighty-ninth lesson in the Javascript-Code module

---
+++ 5-Introduction-to-JavaScript/89-Manipulating_Complex_Objects.js
@@ -0,0 +1,21 @@
+//I learned how to manipulate complex objects using key-value format and also learned about JSON (Javascript Object Notation).
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  // Add record here
+  {
+  "artist": "Daft Punk",
+  "title": "Homework",
+  "release_year": 1997,
+  "formats": ["CD", "Cassette", "LP"]
+  }
+];
