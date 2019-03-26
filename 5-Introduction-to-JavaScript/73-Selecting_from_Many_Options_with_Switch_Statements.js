2c68692ab1d7f29573906fba55a4644bf0e83665
padmalatha kuppa <padmalathakuppa411@gmail.com>
2019-03-25 20:44:10 -0600

    Adds code to the seventy-third lesson in the Javascript-Code module

---
+++ 5-Introduction-to-JavaScript/73-Selecting_from_Many_Options_with_Switch_Statements.js
@@ -0,0 +1,24 @@
+//I learned about switch statement and used it to test four values and set different answers for each case.
+function caseInSwitch(val) {
+  var answer = "";
+  // Only change code below this line
+  switch(val) {
+   case 1:
+   answer = "alpha";
+   break;
+   case 2:
+   answer = "beta";
+   break;
+   case 3:
+   answer = "gamma";
+   break;
+   case 4:
+   answer = "delta";
+   break;
+  }
+  // Only change code above this line
+  return answer;
+}
+
+// Change this value to test
+caseInSwitch(1);
