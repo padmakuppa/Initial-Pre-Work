175e2995d74e32d806021a35c8e5b0e55c67eede
padmalatha kuppa <padmalathakuppa411@gmail.com>
2019-03-26 14:38:48 -0600

    Adds code to the third lesson in the ES6-Challenges module

---
+++ 6-Introduction-to-the-ES6-Challenges/3-Declare-a-Read-Only-Variable-with-the-const-Keyword.js
@@ -0,0 +1,15 @@
+//I learned about the CONST (read-only) variable. Means, the variable cannot be reassigned.
+function printManyTimes(str) {
+  "use strict";
+
+  // change code below this line
+
+  const SENTENCE = str + " is cool!";
+  for(let i = 0; i < str.length; i+=2) {
+    console.log(SENTENCE);
+  }
+
+  // change code above this line
+
+}
+printManyTimes("freeCodeCamp");
