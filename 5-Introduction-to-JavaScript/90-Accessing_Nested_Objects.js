b4e5f7a29a2f359cd836830b4bd87243d68fdd13
padmalatha kuppa <padmalathakuppa411@gmail.com>
2019-03-25 21:07:41 -0600

    Adds code to the ninetieth lesson in the Javascript-Code module

---
+++ 5-Introduction-to-JavaScript/90-Accessing_Nested_Objects.js
@@ -0,0 +1,15 @@
+//I learned how to access sub-properties of Objects by chaining together the dot or bracket notation.
+// Setup
+var myStorage = {
+  "car": {
+    "inside": {
+      "glove box": "maps",
+      "passenger seat": "crumbs"
+     },
+    "outside": {
+      "trunk": "jack"
+    }
+  }
+};
+
+var gloveBoxContents = myStorage.car.inside["glove box"]; // Change this line
