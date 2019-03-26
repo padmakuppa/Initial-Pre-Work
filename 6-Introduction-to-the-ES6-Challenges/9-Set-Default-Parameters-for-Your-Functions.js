f651eabeffe22b37d057f5273aa73797c6cfbe13
padmalatha kuppa <padmalathakuppa411@gmail.com>
2019-03-26 14:44:15 -0600

    Adds code to the ninth lesson in the ES6-Challenges module

---
+++ 6-Introduction-to-the-ES6-Challenges/9-Set-Default-Parameters-for-Your-Functions.js
@@ -0,0 +1,9 @@
+//I learned how to create default parameters in functions.
+const increment = (function() {
+  "use strict";
+  return function increment(number, value = 1) {
+    return number + value;
+  };
+})();
+console.log(increment(5, 2)); // returns 7
+console.log(increment(5)); // returns 6
