32b7bb7058909fdf314be190149295177378b190
padmalatha kuppa <padmalathakuppa411@gmail.com>
2019-03-25 21:15:29 -0600

    Adds code to the ninety-sixth lesson in the Javascript-Code module

--- 5-Introduction-to-JavaScript/96-Count_Backwards_With_a_For_Loop.js
+++ 5-Introduction-to-JavaScript/96-Count_Backwards_With_a_For_Loop.js
@@ -1,1 +1,15 @@
+//I iterated odd numbers backward with a FOR loop and pushed values to an array.
+// Example
+var ourArray = [];
 
+for (var i = 10; i > 0; i -= 2) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+for (i = 9; i > 0; i -= 2) {
+  myArray.push(i);
+}
