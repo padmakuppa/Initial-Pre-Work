6b435ab74c375fe77f349e5227d36c254f46075f
padmalatha kuppa <padmalathakuppa411@gmail.com>
2019-03-25 21:25:45 -0600

    Adds code to the one hundred and fifth lesson in the Javascript-Code module

---
+++ 5-Introduction-to-JavaScript/105-Use_the_parseInt_Function_with_a_Radix.js
@@ -0,0 +1,6 @@
+//I learned about parseInt() function’s second argument radix, and radix can be an integer between 2 and 36. 
+function convertToInteger(str) {
+return  parseInt(str,2);
+}
+
+convertToInteger("10011");
