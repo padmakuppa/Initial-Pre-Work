e0e19c5b23a83e8b4f4cc8b189c8fa5805fc19e9
padmalatha kuppa <padmalathakuppa411@gmail.com>
2019-03-26 14:54:57 -0600

    Adds code to the twenty-second lesson in the ES6-Challenges module

---
+++ 6-Introduction-to-the-ES6-Challenges/22-Understand-the-Differences-Between-import-and-require.js
@@ -0,0 +1,4 @@
+//I learned about IMPORT feature and how it would choose parts of a module or file to load into a given file.
+"use strict";
+import { capitalizeString } from "string_functions"
+capitalizeString("hello!");
