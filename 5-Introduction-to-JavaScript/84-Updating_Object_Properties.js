8724ef02441e0fd9893885c44b9054d59cca8a18
padmalatha kuppa <padmalathakuppa411@gmail.com>
2019-03-25 21:00:28 -0600

    Adds code to the eighty-fourth lesson in the Javascript-Code module

---
+++ 5-Introduction-to-JavaScript/84-Updating_Object_Properties.js
@@ -0,0 +1,22 @@
+//I learned how to update object properties using either dot or bracket notation.
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+ourDog.name = "Happy Camper";
+
+// Setup
+var myDog = {
+  "name": "Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+// Only change code below this line.
+//myDog.name = "Happy Coder";
+myDog["name"] = "Happy Coder";
