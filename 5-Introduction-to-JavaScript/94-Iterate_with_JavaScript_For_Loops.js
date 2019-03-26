47c93d21f75a31ee16f7631beeb934dd2612080f
padmalatha kuppa <padmalathakuppa411@gmail.com>
2019-03-25 21:13:52 -0600

    Adds code to the ninety-fourth lesson in the Javascript-Code module

---
+++ 5-Introduction-to-JavaScript/94-Iterate_with_JavaScript_For_Loops.js
@@ -0,0 +1,15 @@
+//I learned how to iterate with a FOR loop and used to push values to an array.
+// Example
+var ourArray = [];
+
+for (var i = 0; i < 5; i++) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+for (var i = 1; i < 6; i++ ) {
+  myArray.push(i);
+}
