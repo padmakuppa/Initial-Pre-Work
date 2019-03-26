f375de377088891cb367ad96a0253f8b48ea564b
padmalatha kuppa <padmalathakuppa411@gmail.com>
2019-03-25 20:39:16 -0600

    Adds code to the sixty-ninth lesson in the Javascript-Code module

---
+++ 5-Introduction-to-JavaScript/69-Introducing_Else_If_Statements.js
@@ -0,0 +1,14 @@
+//I learned how to use ELSE IF statements to chain IF statements together.
+function testElseIf(val) {
+  if (val > 10) {
+    return "Greater than 10";
+  } else if (val < 5) {
+    return "Smaller than 5";
+  } else {
+
+  return "Between 5 and 10";
+}
+}
+
+// Change this value to test
+testElseIf(7);
