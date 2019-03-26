e1dd4e7cd0b65e042b8dc6f292b4afbbc83ba501
padmalatha kuppa <padmalathakuppa411@gmail.com>
2019-03-25 21:08:35 -0600

    Adds code to the ninety-first lesson in the Javascript-Code module

---
+++ 5-Introduction-to-JavaScript/91-Accessing_Nested_Arrays.js
@@ -0,0 +1,24 @@
+//I learned how to access nested arrays inside the Objects by chaining bracket notation. 
+// Setup
+var myPlants = [
+  {
+    type: "flowers",
+    list: [
+      "rose",
+      "tulip",
+      "dandelion"
+    ]
+  },
+  {
+    type: "trees",
+    list: [
+      "fir",
+      "pine",
+      "birch"
+    ]
+  }
+];
+
+// Only change code below this line
+
+var secondTree = myPlants[1].list[1]; // Change this line
