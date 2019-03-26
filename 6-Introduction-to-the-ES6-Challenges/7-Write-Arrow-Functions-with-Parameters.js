b67f74e379dacd21fc5457928e41100aa1601267
padmalatha kuppa <padmalathakuppa411@gmail.com>
2019-03-26 14:41:36 -0600

    Adds code to the seventh lesson in the ES6-Challenges module

---
+++ 6-Introduction-to-the-ES6-Challenges/7-Write-Arrow-Functions-with-Parameters.js
@@ -0,0 +1,7 @@
+//I learned how to write arrow functions with parameters.
+const myConcat = (arr1, arr2) => {
+  "use strict";
+  return arr1.concat(arr2);
+};
+// test your code
+console.log(myConcat([1, 2], [3, 4, 5]));
