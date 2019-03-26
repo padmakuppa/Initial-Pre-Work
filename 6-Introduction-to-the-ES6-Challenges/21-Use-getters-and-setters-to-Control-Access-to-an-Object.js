92dcec3516c5baf584d24360f04693ba0dc06be6
padmalatha kuppa <padmalathakuppa411@gmail.com>
2019-03-26 14:54:30 -0600

    Adds code to the twenty-first lesson in the ES6-Challenges module

---
+++ 6-Introduction-to-the-ES6-Challenges/21-Use-getters-and-setters-to-Control-Access-to-an-Object.js
@@ -0,0 +1,23 @@
+//I learned about getter and setter to obtain and set values to and from an object.
+function makeClass() {
+  "use strict";
+  /* Alter code below this line */
+ class Thermostat {
+   constructor(fahrenheit){
+    this.fahrenheit = fahrenheit;
+   }
+    get temperature(){
+     return  5 / 9 * (this.fahrenheit - 32)
+    }
+    set temperature(celsius) {
+      return this.fahrenheit = celsius * 9.0 / (5 + 32);
+    }
+ }
+  /* Alter code above this line */
+  return Thermostat;
+}
+const Thermostat = makeClass();
+const thermos = new Thermostat(76); // setting in Fahrenheit scale
+let temp = thermos.temperature; // 24.44 in C
+thermos.temperature = 26;
+temp = thermos.temperature; // 26 in C()
