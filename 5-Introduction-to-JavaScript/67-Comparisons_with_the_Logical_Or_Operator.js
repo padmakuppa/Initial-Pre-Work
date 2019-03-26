168a5d244db4afec17bf28d90016024222ea5a28
padmalatha kuppa <padmalathakuppa411@gmail.com>
2019-03-25 20:37:34 -0600

    Adds code to the sixty-seventh lesson in the Javascript-Code module

---
+++ 5-Introduction-to-JavaScript/67-Comparisons_with_the_Logical_Or_Operator.js
@@ -0,0 +1,15 @@
+//I learned about the Logical-Or (||) operator. The logical ‘or’ operator returns true if either of the operands is true.
+function testLogicalOr(val) {
+  // Only change code below this line
+
+  if (val < 10 || val >20) {
+    return "Outside";
+  }
+
+
+  // Only change code above this line
+  return "Inside";
+}
+
+// Change this value to test
+testLogicalOr(15);
