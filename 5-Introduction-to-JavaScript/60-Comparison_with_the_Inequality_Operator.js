9357825465fddf4fb236eeadb405d4a71a4d5223
padmalatha kuppa <padmalathakuppa411@gmail.com>
2019-03-25 20:28:36 -0600

    Adds code to the sixtieth lesson in the Javascript-Code module

---
+++ 5-Introduction-to-JavaScript/60-Comparison_with_the_Inequality_Operator.js
@@ -0,0 +1,11 @@
+//I learned how to compare with the inequality operator (!=).
+// Setup
+function testNotEqual(val) {
+  if (val != 99) { // Change this line
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+// Change this value to test
+testNotEqual(10);
