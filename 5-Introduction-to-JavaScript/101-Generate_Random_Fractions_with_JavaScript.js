3b192e068808d5a0927ae0c8b5c46c030b15b22c
padmalatha kuppa <padmalathakuppa411@gmail.com>
2019-03-25 21:22:01 -0600

    Adds code to the one hundred and first lesson in the Javascript-Code module

---
+++ 5-Introduction-to-JavaScript/101-Generate_Random_Fractions_with_JavaScript.js
@@ -0,0 +1,9 @@
+//I learned about Math.random() function, and it would generate a random decimal number between 0(inclusive)-1(exclusive).
+function randomFraction() {
+
+  // Only change code below this line.
+
+  return Math.random();
+
+  // Only change code above this line.
+}
