8a97acb7e5dcb5a57d7de1f24b6648a71d953b3b
padmalatha kuppa <padmalathakuppa411@gmail.com>
2019-03-26 14:53:34 -0600

    Adds code to the nineteenth lesson in the ES6-Challenges module

---
+++ 6-Introduction-to-the-ES6-Challenges/19-Write-Concise-Declarative-Functions-with-ES6.js
@@ -0,0 +1,12 @@
+//I learned how to write concise declarative functions inside the objects without using the function keyword. 
+// change code below this line
+const bicycle = {
+  gear: 2,
+  setGear(newGear) {
+    "use strict";
+    this.gear = newGear;
+  }
+};
+// change code above this line
+bicycle.setGear(3);
+console.log(bicycle.gear);
