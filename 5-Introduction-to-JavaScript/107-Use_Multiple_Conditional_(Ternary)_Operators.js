453afc94855bef32c7b4a3fc95ab19df309b6513
padmalatha kuppa <padmalathakuppa411@gmail.com>
2019-03-25 21:26:42 -0600

    Adds code to the one hundred and seventh lesson in the Javascript-Code module

---
+++ 5-Introduction-to-JavaScript/107-Use_Multiple_Conditional_(Ternary)_Operators.js
@@ -0,0 +1,6 @@
+//I learned how to chain multiple conditional (ternary) operators to check the conditions.
+function checkSign(num) {
+ return num > 0 ? "positive" : num < 0 ? "negative" : "zero";
+}
+
+checkSign(10);
