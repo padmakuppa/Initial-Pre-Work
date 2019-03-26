e15dfeed602b99a35f517cd857ced263623a1f75
padmalatha kuppa <padmalathakuppa411@gmail.com>
2019-03-25 21:14:43 -0600

    Adds code to the ninety-fifth lesson in the Javascript-Code module

---
+++ 5-Introduction-to-JavaScript/95-Iterate_Odd_Numbers_With_a_For_Loop.js
@@ -0,0 +1,15 @@
+//I iterated odd numbers with a FOR loop and pushed values to an array.
+// Example
+var ourArray = [];
+
+for (var i = 0; i < 10; i += 2) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+for (i = 1; i < 10; i += 2) {
+  myArray.push(i);
+}
